c459ff5f64d2502e6bf03d5b7f3eb1c4fe2ef8f6
Vinishko Chan <nikitasaviczkij@gmail.com>
2021-05-25 20:55:17 +0300

    fix: remove unused days

--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -3,19 +3,17 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.css']
+  styleUrls: ['./schedule.component.css'],
 })
 export class ScheduleComponent implements OnInit {
-
   @Input()
   schedule: any;
-  numberOfDays: number[] = [1, 2, 3, 4, 5, 6];
+  numberOfDays: number[] = [1, 2];
   numberOfGroups: string[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.numberOfGroups = Object.keys(this.schedule["1"]);
+    this.numberOfGroups = Object.keys(this.schedule['1']);
   }
-
 }
